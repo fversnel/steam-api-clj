1d438cf0232a8c8a262ae4095d61f394bea21457
Frank Versnel <f.versnel@gmail.com>
2015-07-23 16:51:52 +0200

    Updated project url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject steam-api-clj "0.1.0-SNAPSHOT"
   :description "Steam Web API interface for Clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/fversnel/steam-api-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]

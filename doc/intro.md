53a107913792d07be794f1b2023fe0b932fe37f9
Frank Versnel <f.versnel@gmail.com>
2015-07-24 08:35:18 +0200

    Removed default test and docs.

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to steam-api-clj
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- test/steam_api_clj/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns steam-api-clj.core-test
-  (:require [clojure.test :refer :all]
-            [steam-api-clj.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))

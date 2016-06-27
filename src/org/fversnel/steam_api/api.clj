ec9ad0fc10bbd4340ce2afb8288035242fcaee55
Frank Versnel <f.versnel@gmail.com>
2016-06-27 17:57:31 +0200

    Code cleanup

--- src/org/fversnel/steam_api/api.clj
+++ src/org/fversnel/steam_api/api.clj
@@ -6,20 +6,20 @@
    "vdf"  "text/vdf"
    "csv"  "text/csv"})
 
-(defn to-indexed-param
+(defn to-indexed-array
   "Creates an indexed array of parameter value pairs to be
    used in the parameter list of some of the Steam web requests.
 
    Example:
    (to-indexed-param-array \"test\" [42 43 44])
    => {\"test[0]\" 42 \"test[1]\" 43 \"test[2]\" 44}"
-  [array-name values]
+  [param-name values]
   (if-not (nil? values)
     (->> (or values [])
-         (map-indexed (fn [index value] [(str array-name "[" index "]") value]))
+         (map-indexed (fn [index value] [(str param-name "[" index "]") value]))
          (into {}))))
 
-(defn to-normal-param
+(defn to-normal-value
   [param-name value]
   (cond
     (coll? value) [param-name (clojure.string/join "," value)]
@@ -37,8 +37,8 @@
                                       (first description)
                                       :normal)
                           conform (case data-type
-                                    :normal `to-normal-param
-                                    :indexed-array `to-indexed-param)]
+                                    :normal `to-normal-value
+                                    :indexed-array `to-indexed-array)]
                       `(~conform ~(name param-key) ~param-symbol)))
         param-names# (keys param-specs#)
         param-symbols# (map (comp symbol name) param-names#)
